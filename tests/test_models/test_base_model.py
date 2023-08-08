38d91633301e78c01f9931653e7ca1440b32468c
patrice02 <patricedev02@gmail.com>
2023-08-08 13:12:58 +0000

    test base class attributes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,14 +4,16 @@ Unit test for BaseModel class
 """
 
 import unittest
-import datetime
+from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModelClassDocstrings(unittest.TestCase):
+    """Tests Docstrings"""
 
     @classmethod
     def setUpClass(cls):
+        """Add attributs to Test class"""
         _cls = BaseModel()
         docs_list = []
         docs_list.append(_cls.__module__.__doc__)
@@ -24,27 +26,46 @@ class TestBaseModelClassDocstrings(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        """Remove unused attributs from Test class"""
         del cls.docs_list
 
     def test_module_docstring(self):
+        """Test docstring"""
         for docstring in self.__class__.docs_list:
             with self.subTest(docstring=docstring):
                 self.assertTrue(len(docstring) > 10)
 
 
 class TestBaseClass(unittest.TestCase):
-    
+    """Tests BaseClass"""
 
     def test_instantiation(self):
+        """Tests BaseModel instantion"""
         base = BaseModel()
-        self.assertTrue(str(type(bm)),
+        self.assertTrue(str(type(base)),
                          "<class 'models.base_model.BaseModel'>")
         self.assertIsInstance(base, BaseModel)
         self.assertTrue(issubclass(type(base)), BaseModel)
 
     def test_instantiation_with_args(self):
-        # with self.assertRaises()
-        base = BaseModel('1458-876-668', datetime.now(), datetime.now())
+        """Tests __init__ with argument"""
+        msg = 'BaseModel.__init__() takes 1 positional argument but 4 were given'
+        with self.assertRaises(TypeEror, msg=msg):
+            base = BaseModel('1458-876-668', datetime.now(), datetime.now())
+
+    def test_class_attributes(self):
+        """Tests attributes id, create_at, update_at"""
+        base = BaseModel()
+        attrs = [
+        (base, 'id'),
+        (base, 'created_at'),
+        (base, 'updated_at'),
+        (base, 'save'),
+        (base, 'to_dict')
+        ]
+        for ele in attrs:
+            with self.subTest():
+                self.assertTrue(hasattr(ele[0]), ele[1])
 
 
 
